22aa7e8e4a0c2d52c53a04bca93a633c6f32809c
dma004 <david.makowski@gen-p-soft.com>
2020-03-03 12:34:16 +0100

    basic Layout, left menu added

---
+++ StartProject/Assets/Scripts/MenuButton_Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class MenuButton_Controller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    GameObject default_visuals;
+    GameObject selected_visuals;
+
+    public GameObject icon_default;
+    public GameObject icon_hover;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        default_visuals = gameObject.transform.Find("Default").gameObject;
+        selected_visuals = gameObject.transform.Find("Selected").gameObject;
+
+        selected_visuals.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        icon_hover.SetActive(true);
+        icon_default.SetActive(false);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        icon_hover.SetActive(false);
+        icon_default.SetActive(true);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        default_visuals.SetActive(false);
+        selected_visuals.SetActive(true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        default_visuals.SetActive(true);
+        selected_visuals.SetActive(false);
+    }
+
+    
+}
